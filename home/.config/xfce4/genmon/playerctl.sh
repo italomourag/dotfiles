63505180bf8a7e7944875ba09eda871941259467
italomourag <italo_mg@hotmail.com>
2024-10-01 02:01:09 -0300

    novo playerctl.sh: no genmon

---
+++ home/.config/xfce4/genmon/playerctl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Verifica o status do player (se está tocando ou pausado)
+status=$(playerctl status 2>/dev/null)
+
+# Se estiver tocando, exibe a música e o artista
+if [ "$status" = "Playing" ]; then
+    artist=$(playerctl metadata artist)
+    title=$(playerctl metadata title)
+    echo "<txt>  <span background='#269cb6' foreground='#444444' font_family='Roboto' font_weight='bold'>  ▶️  $artist - $title  </span>  </txt>"
+    echo "<tool>$artist - $title</tool>"
+elif [ "$status" = "Paused" ]; then
+    # Exibe pausa
+    echo "<txt>  <span background='#444444' foreground='#e7e8eb' font_family='Roboto' font_weight='bold'>  ⏸️  </span>  </txt>"
+    echo "<tool>Pausado</tool>"
+else
+    # Exibe ícone de stop
+    echo "<txt><span background='#444444' foreground='#e7e8eb' font_family='Roboto' font_weight='bold'></span></txt>"
+    echo "<tool>Nenhuma música tocando</tool>"
+fi
